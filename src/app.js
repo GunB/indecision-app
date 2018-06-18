448f0900245491a692dec787a4e160c73606c84c
Heiner Angarita <heinerangarita@gmail.com>
2018-06-18 11:15:06 -0500

    Creating a more build up project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
 import "./styles/styles.scss";
 import "normalize.css/normalize.css";
 
-ReactDOM.render(
-  <IndecisionApp options={[{ text: "2" }, { text: "1" }, { text: "Magic" }]} />,
-  document.getElementById("app")
-);
+ReactDOM.render(<p />, document.getElementById("app"));
