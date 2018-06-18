920720b11b153eb3bff81bf10351ad111d0d7197
Heiner Angarita <heinerangarita@gmail.com>
2018-06-18 14:48:58 -0500

    Portfolio router

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import "normalize.css/normalize.css";
+import AppRouter from "./routers/AppRouter";
 
-ReactDOM.render(<p />, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
